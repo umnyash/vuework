fa7de44ce9aae53da48b82b0a0d0051f574dd21e
Rustam Sadreev <rustam17.sb@gmail.com>
2025-10-22 21:01:40 +0300

    feat: add common constansts and helper functions

---
+++ frontend/src/common/helpers.js
@@ -0,0 +1,27 @@
+import { DAY_IN_MILLISEC, TAG_SEPARATOR } from "./constants";
+import { PriorityStatus, TimeStatus } from "./enums";
+
+const parseTags = (tags) => tags.split(TAG_SEPARATOR).slice(1);
+
+const determineTimeStatus = (dueDate) => {
+  if (!dueDate) {
+    return "";
+  }
+
+  const currentTime = Date.now();
+  const taskTime = Date.parse(dueDate);
+  const timeDelta = taskTime - currentTime;
+
+  if (timeDelta > DAY_IN_MILLISEC) {
+    return "";
+  }
+
+  return timeDelta > 0 ? TimeStatus.DEADLINE : TimeStatus.EXPIRED;
+};
+
+export const normalizeTask = (task) => ({
+  ...task,
+  tags: parseTags(task.tags),
+  priority: PriorityStatus[task.statusId] ?? "",
+  timeStatus: determineTimeStatus(task.dueDate),
+});
