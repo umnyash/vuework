2275d32d5ba4d9fe4c0bd030582498a7f788719f
Rustam Sadreev <rustam17.sb@gmail.com>
2025-10-22 21:28:37 +0300

    feat: add AppLayout and AppLayoutHeader components

---
+++ frontend/src/layouts/AppLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="app-layout">
+    <app-layout-header />
+    <slot />
+  </div>
+</template>
+
+<script setup>
+import AppLayoutHeader from "./AppLayoutHeader.vue";
+</script>
+
+<style lang="scss" scoped>
+.app-layout {
+  display: flex;
+  height: 100vh;
+  flex-direction: column;
+}
+</style>
