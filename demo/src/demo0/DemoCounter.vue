f35337c892893f7425f4dddb93fe7d6dfb3fbfd9
Rustam Sadreev <rustam17.sb@gmail.com>
2025-10-22 20:47:29 +0300

    feat: add DemoCounter component

---
+++ demo/src/demo0/DemoCounter.vue
@@ -0,0 +1,44 @@
+<template>
+  <demo-container>
+    <div class="counter">
+      <button @click="decrement">-</button>
+
+      <span> Current value: {{ count }} </span>
+
+      <button @click="increment">+</button>
+    </div>
+  </demo-container>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import DemoContainer from "../components/DemoContainer.vue";
+
+const count = ref(0);
+
+const decrement = () => count.value--;
+const increment = () => count.value++;
+</script>
+
+<style lang="scss" scoped>
+.counter {
+  display: flex;
+  align-items: center;
+}
+
+button {
+  width: 2rem;
+  height: 2rem;
+  border: none;
+  border-radius: 50%;
+  margin: 0 10px;
+  font-size: 1.5rem;
+  background: #dd0000;
+  color: white;
+  cursor: pointer;
+
+  &:hover {
+    background: #ff0000;
+  }
+}
+</style>
