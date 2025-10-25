60c0154a4a2d4217ce6e686002b7320cb1a5a249
Rustam Sadreev <rustam17.sb@gmail.com>
2025-10-25 15:35:29 +0300

    refactor: add TaskCardTags component

---
+++ frontend/src/modules/tasks/components/TaskCardTags.vue
@@ -0,0 +1,67 @@
+<template>
+  <ul class="task__tags">
+    <li v-for="(tag, index) in tags" :key="index">
+      <span class="tag tag--blue">{{ tag }}</span>
+    </li>
+  </ul>
+</template>
+
+<script setup>
+const { tags } = defineProps({
+  tags: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/app.scss";
+
+.task__tags {
+  @include clear-list;
+
+  display: flex;
+  flex-wrap: wrap;
+
+  width: 100%;
+  margin-top: 5px;
+
+  li {
+    margin-top: 4px;
+    margin-right: 4px;
+  }
+}
+
+.tag {
+  @include r-s10-h12;
+
+  padding: 4px 8px;
+
+  border-radius: 100px;
+
+  &--pink {
+    background-color: $pink-300;
+  }
+
+  &--orange {
+    background-color: $yellow-300;
+  }
+
+  &--green {
+    background-color: $green-100;
+  }
+
+  &--yellow {
+    background-color: $yellow-100;
+  }
+
+  &--blue {
+    background-color: $blue-300;
+  }
+
+  &--violet {
+    background-color: $pink-200;
+  }
+}
+</style>
