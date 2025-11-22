54f0c7b313dbd47e587e161f5d0e97230fdfe1b3
Rustam Sadreev <rustam17.sb@gmail.com>
2025-11-22 20:01:54 +0300

    feat: add TaskForm component

---
+++ frontend/src/modules/tasks/components/TaskForm.vue
@@ -0,0 +1,638 @@
+<template>
+  <form class="task-card" method="post">
+    <section class="task-card__wrapper">
+      <button class="task-card__close" type="button"></button>
+
+      <h1 class="task-card__name">
+        <span>Название задачи</span>
+        <input
+          type="text"
+          name="task_name"
+          maxlength="37"
+          value="Название задачи"
+        />
+        <app-icon class="icon--edit" />
+      </h1>
+      <p class="task-card__date">#123456 создана 10 минут назад</p>
+
+      <ul class="task-card__params">
+        <li>
+          Участник:
+          <div class="task-card__participant">
+            <button type="button" class="task-card__link">
+              добавить пользователя
+            </button>
+
+            <div class="task-card__users">
+              <ul class="users-list">
+                <li>
+                  <button type="button" class="users-list__user">
+                    <img
+                      :src="getImage('user1.jpg')"
+                      alt="Вика Некрасова"
+                      width="30"
+                      height="30"
+                    />
+                    Вика Некрасова
+                  </button>
+                </li>
+                <li>
+                  <button type="button" class="users-list__user">
+                    <img
+                      :src="getImage('user2.jpg')"
+                      alt="Петр Хрустиков"
+                      width="30"
+                      height="30"
+                    />
+                    Петр Хрустиков
+                  </button>
+                </li>
+                <li>
+                  <button type="button" class="users-list__user">
+                    <img
+                      :src="getImage('user3.jpg')"
+                      alt="Михаил Валерьевич Тян-Шанский"
+                      width="30"
+                      height="30"
+                    />
+                    Михаил Валерьевич Тян-Шанский
+                  </button>
+                </li>
+                <li>
+                  <button type="button" class="users-list__user">
+                    <span>N</span>
+                    Наталья Железнова
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </li>
+        <li>
+          Срок:
+          <button type="button" class="task-card__link">установить срок</button>
+        </li>
+      </ul>
+
+      <div class="task-card__description">
+        <h2 class="task-card__title">Описание</h2>
+        <app-text-area
+          :model-value="''"
+          name="task_description"
+          placeholder="Добавьте описание к задаче"
+        />
+      </div>
+
+      <div class="task-card__links">
+        <h2 class="task-card__title">
+          Ссылки <button type="button" class="task-card__plus"></button>
+        </h2>
+
+        <div class="task-card__links-item">
+          <input type="text" name="task_link_url" placeholder="Введите url" />
+          <input
+            type="text"
+            name="task_link_desc"
+            placeholder="Добавьте описание к ссылке"
+          />
+        </div>
+      </div>
+
+      <div class="task-card__files">
+        <h2 class="task-card__title">Файлы</h2>
+        <label class="task-card__plus task-card__plus--circle">
+          <input type="file" name="add_file" />
+        </label>
+      </div>
+
+      <task-checklist />
+
+      <div class="task-card__buttons">
+        <button type="button" class="button button--border">Отменить</button>
+        <button type="submit" class="button">Сохранить</button>
+      </div>
+    </section>
+  </form>
+</template>
+
+<script setup>
+import { getImage } from "@/common/helpers";
+import AppIcon from "@/common/components/AppIcon.vue";
+import AppTextArea from "@/common/components/AppTextArea.vue";
+import TaskChecklist from "@/modules/tasks/components/TaskChecklist.vue";
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/app.scss";
+
+.button {
+  @include m-s14-h21;
+
+  display: block;
+
+  box-sizing: border-box;
+  width: 196px;
+  margin: 0;
+  padding: 17px 0;
+
+  cursor: pointer;
+  transition: 0.3s;
+  text-align: center;
+  text-transform: uppercase;
+
+  color: $white-900;
+  border: none;
+  border-radius: 6px;
+  outline: none;
+  background-color: $blue-600;
+  box-shadow: 0 4px 8px $shadow-500;
+
+  &:hover:not(:active) {
+    background-color: $blue-500;
+    box-shadow: 0 8px 16px $shadow-600;
+  }
+
+  &:active {
+    background-color: $blue-700;
+    box-shadow: 0 4px 8px $shadow-500;
+  }
+
+  &--border {
+    color: $blue-gray-600;
+    border: 1px solid $blue-gray-600;
+    background-color: $white-900;
+
+    &:hover:not(:active) {
+      background-color: $white-900;
+    }
+
+    &:active {
+      background-color: $white-700;
+    }
+  }
+
+  &--disabled {
+    pointer-events: none;
+
+    opacity: 0.5;
+  }
+}
+
+.task-card {
+  $bl: ".task-card";
+
+  position: fixed;
+  z-index: 100;
+  top: 0;
+  left: 0;
+
+  overflow-x: auto;
+
+  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
+  padding-top: 50px;
+  padding-bottom: 50px;
+
+  background: $gray-900;
+
+  &__close {
+    position: absolute;
+    top: 16px;
+    right: 16px;
+
+    width: 20px;
+    height: 20px;
+
+    cursor: pointer;
+
+    border: none;
+    outline: none;
+    background-color: transparent;
+
+    &::after,
+    &::before {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+
+      width: 100%;
+      height: 1px;
+
+      content: "";
+      transition: background-color 0.3s;
+
+      background-color: $blue-gray-300;
+    }
+
+    &::after {
+      transform: translate(-50%, -50%) rotate(45deg);
+    }
+
+    &::before {
+      transform: translate(-50%, -50%) rotate(-45deg);
+    }
+
+    &:hover {
+      &::before,
+      &::after {
+        background-color: $blue-700;
+      }
+    }
+  }
+
+  &__wrapper {
+    position: relative;
+
+    width: 500px;
+    margin: 0 auto;
+    padding: 40px;
+
+    background-color: $white-900;
+    box-shadow: 0 4px 8px $shadow-500;
+  }
+
+  &__name,
+  &__name input {
+    @include m-s24-h21;
+
+    display: block;
+
+    width: 100%;
+    margin: 0;
+    padding: 0;
+    padding-bottom: 5px;
+
+    color: $gray-900;
+    border: none;
+    border-bottom: 1px solid transparent;
+    border-radius: 0;
+    outline: none;
+
+    &:focus:not(:disabled) {
+      cursor: text;
+
+      border-color: $blue-gray-200;
+    }
+  }
+
+  &__name {
+    button,
+    span {
+      display: inline-block;
+
+      vertical-align: middle;
+    }
+
+    button {
+      margin-left: 11px;
+    }
+
+    input {
+      display: none;
+    }
+  }
+
+  &__date {
+    @include r-s14-h21;
+
+    margin-top: 5px;
+    margin-bottom: 40px;
+
+    color: $blue-gray-300;
+  }
+
+  &__params {
+    @include clear-list;
+    @include r-s16-h21;
+
+    margin-bottom: 40px;
+
+    color: $gray-900;
+
+    li {
+      margin-bottom: 24px;
+    }
+  }
+
+  &__participant {
+    position: relative;
+
+    display: inline-block;
+
+    vertical-align: baseline;
+  }
+
+  &__users {
+    position: absolute;
+    z-index: 10;
+    top: -12px;
+    left: 0;
+
+    display: none;
+
+    box-sizing: border-box;
+    width: 210px;
+
+    border-radius: 6px;
+    background-color: $white-900;
+    box-shadow: 0 4px 8px $shadow-500;
+  }
+
+  &__user {
+    @include r-s14-h16;
+
+    display: inline-flex;
+    align-items: center;
+
+    width: 100%;
+    margin: 0;
+    margin-left: 5px;
+    padding: 0;
+
+    cursor: pointer;
+    transform: translateY(-2px);
+    text-align: left;
+    vertical-align: middle;
+
+    border: none;
+    outline: none;
+    background-color: transparent;
+
+    &::after {
+      position: absolute;
+      top: 8px;
+      right: -20px;
+
+      width: 14px;
+      height: 14px;
+
+      content: "";
+      transition: opacity 0.3s;
+
+      opacity: 0;
+      background-image: url("@/assets/img/icon-pencil.svg");
+      background-size: cover;
+    }
+
+    &:hover {
+      &::after {
+        opacity: 1;
+      }
+    }
+
+    img {
+      width: 30px;
+      height: 30px;
+      margin-right: 10px;
+
+      border-radius: 50%;
+    }
+
+    span {
+      @include m-s14-h21;
+
+      display: block;
+
+      box-sizing: border-box;
+      width: 30px;
+      height: 30px;
+      margin-right: 10px;
+      padding-top: 5px;
+
+      text-align: center;
+
+      color: $white-900;
+      border-radius: 50%;
+      background-color: $green-700;
+    }
+  }
+
+  &__link {
+    @include r-s16-h21;
+
+    position: relative;
+
+    margin: 0;
+    padding: 0;
+    padding-right: 23px;
+
+    cursor: pointer;
+    text-decoration: underline;
+
+    color: $blue-gray-600;
+    border: none;
+    outline: none;
+    background-color: transparent;
+
+    &::after {
+      position: absolute;
+      top: 2px;
+      right: 0;
+
+      width: 14px;
+      height: 14px;
+
+      content: "";
+      transition: opacity 0.3s;
+
+      opacity: 0;
+      background-image: url("@/assets/img/icon-pencil.svg");
+      background-size: cover;
+    }
+
+    &:hover {
+      text-decoration: none;
+
+      &::after {
+        opacity: 1;
+      }
+    }
+
+    &--text {
+      text-decoration: none;
+
+      color: $gray-900;
+
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+
+  :deep(#{$bl}__title) {
+    @include m-s18-h21;
+
+    margin: 0;
+
+    color: $gray-900;
+  }
+
+  &__description {
+    margin-bottom: 40px;
+
+    p {
+      margin-bottom: 10px;
+    }
+  }
+
+  :deep(#{$bl}__plus) {
+    display: inline-block;
+
+    width: 14px;
+    height: 14px;
+    margin: 0;
+    padding: 0;
+
+    cursor: pointer;
+    transition: opacity 0.3s;
+    vertical-align: middle;
+
+    opacity: 0.8;
+    border: none;
+    border-radius: 50%;
+    outline: none;
+    background-color: transparent;
+    background-image: url("@/assets/img/icon-plus.svg");
+    background-size: cover;
+
+    &:hover {
+      opacity: 1;
+    }
+  }
+
+  :deep(#{$bl}__plus--circle) {
+    width: 30px;
+    height: 30px;
+    margin-top: 10px;
+
+    background-image: url("@/assets/img/icon-add.svg");
+  }
+
+  &__links-item {
+    margin-top: 16px;
+
+    border-bottom: 1px solid $blue-gray-300;
+
+    &--border_none {
+      border-bottom: 0;
+    }
+
+    input {
+      @include r-s14-h21;
+
+      display: block;
+
+      width: 100%;
+      margin-bottom: 10px;
+
+      color: $blue-gray-600;
+      border: none;
+      outline: none;
+    }
+
+    a {
+      @include r-s14-h21;
+
+      display: block;
+
+      margin-bottom: 10px;
+      padding-right: 24;
+
+      color: $blue-600;
+
+      &::after {
+        display: inline-block;
+
+        width: 14px;
+        height: 14px;
+        margin-left: 10px;
+
+        content: "";
+        transition: opacity 0.3s;
+        vertical-align: baseline;
+
+        opacity: 0;
+        background-image: url("@/assets/img/icon-pencil.svg");
+        background-size: cover;
+      }
+
+      &:hover {
+        text-decoration: underline;
+
+        &::after {
+          opacity: 1;
+        }
+      }
+    }
+  }
+
+  &__files {
+    margin-top: 30px;
+    margin-bottom: 20px;
+
+    input {
+      display: none;
+    }
+
+    p {
+      @include r-s14-h21;
+
+      margin-top: 10px;
+      margin-bottom: 0;
+
+      color: $blue-gray-300;
+    }
+  }
+
+  &__list {
+    @include clear-list;
+
+    margin-top: 15px;
+  }
+
+  &__item {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    margin-top: 10px;
+
+    &:hover {
+      #{$bl}__icons {
+        opacity: 1;
+      }
+    }
+  }
+
+  &__checkbox {
+    flex-grow: 1;
+
+    margin-right: 20px;
+  }
+
+  &__icons {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    width: 50px;
+
+    transition: opacity 0.3s;
+
+    opacity: 0;
+  }
+
+  &__buttons {
+    display: flex;
+    justify-content: space-between;
+
+    width: 410px;
+    margin-top: 50px;
+  }
+
+  &__comments {
+    margin-top: 30px;
+  }
+}
+</style>
