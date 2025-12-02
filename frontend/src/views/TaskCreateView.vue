7cde4b188a7d837e5854fbe91e9e443b543d419c
Rustam Sadreev <rustam17.sb@gmail.com>
2025-12-02 04:12:06 +0300

    feat: implement task creation

--- frontend/src/views/TaskCreateView.vue
+++ frontend/src/views/TaskCreateView.vue
@@ -1,7 +1,9 @@
 <template>
-  <task-form />
+  <task-form @submit="emit('taskFormSubmit', $event)" />
 </template>
 
 <script setup>
 import TaskForm from "@/modules/tasks/components/TaskForm.vue";
+
+const emit = defineEmits(["taskFormSubmit"]);
 </script>
