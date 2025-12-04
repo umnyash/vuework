56c70fad131c07026c31e8521343ebaa0208d1c3
Rustam Sadreev <rustam17.sb@gmail.com>
2025-12-04 03:42:22 +0300

    feat: add DemoCounter with state

---
+++ demo/src/demo4/DemoCounterButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <button class="counter-button" @click="$emit('action')">
+    <slot />
+  </button>
+</template>
+
+<style lang="scss" scoped>
+.counter-button {
+  width: 2rem;
+  height: 2rem;
+  border: none;
+  border-radius: 50%;
+  margin: 0 10px;
+  font-size: 1.5rem;
+  background: #dd0000;
+  color: white;
+  cursor: pointer;
+
+  &:hover {
+    background: #ff0000;
+  }
+
+  &[disabled] {
+    background: #999999;
+    cursor: not-allowed;
+  }
+}
+</style>
