8f07ec1343650561c714829a2143688d86eaa59c
Rustam Sadreev <rustam17.sb@gmail.com>
2025-12-15 01:59:41 +0300

    feat: add auth check on app start

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import { outsideClick } from "./common/directives";
+import { getToken, removeToken } from "@/services/token-manager";
+import { useAuthStore } from "@/stores";
 
 import App from "./App.vue";
 import router from "./router";
@@ -12,3 +14,17 @@ app.use(router);
 app.directive("outside-click", outsideClick);
 
 app.mount("#app");
+
+const token = getToken();
+
+if (token) {
+  const authStore = useAuthStore();
+  authStore
+    .getMe()
+    .then(() => {
+      router.push("/");
+    })
+    .catch(() => {
+      removeToken();
+    });
+}
